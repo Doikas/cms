2f51ea5f7ff4f8187ad6d4318b4b7eac76391e69
Doikas <stevekdoikas@hotmail.com>
2022-05-29 13:17:56 +0300

    User Login

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,4 @@
 <?php include "includes/admin_header.php" ?>
-
     <div id="wrapper">
 
 
@@ -16,7 +15,8 @@
                     <div class="col-lg-12">
                         <h1 class="page-header">
                             Welcome to admin
-                            <small>Author</small>
+                            
+                            <small><?php echo $_SESSION['username']; ?></small>
                         </h1>
                     </div>
                 </div>
