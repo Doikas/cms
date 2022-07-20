f4f6c235dea0f8ad64bc882532aac780c244ad80
Doikas <stevekdoikas@hotmail.com>
2022-07-20 16:42:26 +0300

    Like and unlike feature to posts

--- admin/functions.php
+++ admin/functions.php
@@ -1,200 +1,209 @@
-<?php 
-function confirmQuery($result) {
+<?php
+function confirmQuery($result)
+{
     global $connection;
-    if(!$result){
+    if (!$result) {
         die("QUERY FAILED" . mysqli_error($connection));
     }
-
 }
 
-function insert_categories() {
+function insert_categories()
+{
     global $connection;
-    if(isset($_POST['submit'])){
+    if (isset($_POST['submit'])) {
         $cat_title = $_POST['cat_title'];
-    
-        if($cat_title == "" || empty($cat_title)){
+
+        if ($cat_title == "" || empty($cat_title)) {
             echo "This field shouldn't be empty ";
-        }else{
+        } else {
             $query = "INSERT INTO categories(cat_title) ";
             $query .= "VALUE('{$cat_title}') ";
             $create_category_query = mysqli_query($connection, $query);
-            if(!$create_category_query){
-                die('QUERY FAILED'. mysqli_error($connection));
+            if (!$create_category_query) {
+                die('QUERY FAILED' . mysqli_error($connection));
             }
-    
         }
     }
-    
 }
 
-function findallCategories() {
-global $connection;
-$query = "SELECT * FROM categories";
-$select_categories = mysqli_query($connection, $query);
-while($row = mysqli_fetch_assoc($select_categories)){
-$cat_id = $row['cat_id'];
-$cat_title = $row['cat_title'];
-echo "<tr>";
-echo "<td>{$cat_id}</td>";
-echo "<td>{$cat_title}</td>";
-echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-echo "</tr>";
-}  
-}
-
-function DeleteCategories() {
-global $connection;
-if(isset($_GET['delete'])){
-    $the_cat_id = $_GET['delete'];
-    $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
-    $delete_query = mysqli_query($connection, $query);
-    header("Location: categories.php");
-}
-}
-
-function UpdateCategories() {
-global $connection;  
-if(isset($_GET['edit'])){
-    $cat_id = $_GET['edit'];
-    include "includes/update_categories.php";
-}
-}
-
-function users_online(){
-
-    if(isset($_GET['onlineusers'])){
-
-    
+function findallCategories()
+{
     global $connection;
-    if(!$connection){
-        session_start();
-        include("../includes/db.php");
-   
-    $session = session_id();
-    $time = time();
-    $time_out_in_seconds = 05;
-    $time_out = $time - $time_out_in_seconds;
-    $query = "SELECT * FROM users_online WHERE session = '$session'";
-    $send_query = mysqli_query($connection, $query);
-    $count = mysqli_num_rows($send_query);
-    if($count == NULL){
-        mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session','$time')");
-    } else {
-        mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+    $query = "SELECT * FROM categories";
+    $select_categories = mysqli_query($connection, $query);
+    while ($row = mysqli_fetch_assoc($select_categories)) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+        echo "<tr>";
+        echo "<td>{$cat_id}</td>";
+        echo "<td>{$cat_title}</td>";
+        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+        echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+        echo "</tr>";
     }
-    $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out'");
-    echo $count_user = mysqli_num_rows($users_online_query);
 }
+
+function DeleteCategories()
+{
+    global $connection;
+    if (isset($_GET['delete'])) {
+        $the_cat_id = $_GET['delete'];
+        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+        $delete_query = mysqli_query($connection, $query);
+        header("Location: categories.php");
+    }
 }
+
+function UpdateCategories()
+{
+    global $connection;
+    if (isset($_GET['edit'])) {
+        $cat_id = $_GET['edit'];
+        include "includes/update_categories.php";
+    }
+}
+
+function users_online()
+{
+
+    if (isset($_GET['onlineusers'])) {
+
+
+        global $connection;
+        if (!$connection) {
+            session_start();
+            include("../includes/db.php");
+
+            $session = session_id();
+            $time = time();
+            $time_out_in_seconds = 05;
+            $time_out = $time - $time_out_in_seconds;
+            $query = "SELECT * FROM users_online WHERE session = '$session'";
+            $send_query = mysqli_query($connection, $query);
+            $count = mysqli_num_rows($send_query);
+            if ($count == NULL) {
+                mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session','$time')");
+            } else {
+                mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+            }
+            $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out'");
+            echo $count_user = mysqli_num_rows($users_online_query);
+        }
+    }
 }
 users_online();
 
-function escape($string) {
-  global $connection;
-  return  mysqli_real_escape_string($connection, trim($string));
-}
-function recordCount($table){
-global $connection;
-$query ="SELECT * FROM " . $table;
-$select_all_posts = mysqli_query($connection,$query);
-$result = $post_count = mysqli_num_rows($select_all_posts);
-confirmQuery($result);
-return $result;
-}
-
-function checkStatus($table,$column,$status){
+function escape($string)
+{
     global $connection;
-    $query ="SELECT * FROM $table WHERE $column = '$status' ";
-    $result = mysqli_query($connection,$query);
-    return mysqli_num_rows($result);
-
+    return  mysqli_real_escape_string($connection, trim($string));
+}
+function recordCount($table)
+{
+    global $connection;
+    $query = "SELECT * FROM " . $table;
+    $select_all_posts = mysqli_query($connection, $query);
+    $result = $post_count = mysqli_num_rows($select_all_posts);
+    confirmQuery($result);
+    return $result;
 }
 
-function is_admin($username){
+function checkStatus($table, $column, $status)
+{
+    global $connection;
+    $query = "SELECT * FROM $table WHERE $column = '$status' ";
+    $result = mysqli_query($connection, $query);
+    return mysqli_num_rows($result);
+}
+
+function is_admin($username)
+{
     global $connection;
     $query = "SELECT user_role FROM users WHERE username = '$username'";
     $result = mysqli_query($connection, $query);
     confirmQuery($result);
     $row  = mysqli_fetch_array($result);
-    if($row['user_role'] == 'admin'){
+    if ($row['user_role'] == 'admin') {
         return true;
     } else {
         return false;
     }
-
 }
 
-function username_exists($username){
+function username_exists($username)
+{
     global $connection;
     $query = "SELECT username FROM users WHERE username = '$username'";
     $result = mysqli_query($connection, $query);
     confirmQuery($result);
 
-    if(mysqli_num_rows($result) > 0){
+    if (mysqli_num_rows($result) > 0) {
         return true;
     } else {
         return false;
     }
-
 }
 
-function email_exists($email){
+function email_exists($email)
+{
     global $connection;
     $query = "SELECT user_email FROM users WHERE user_email = '$email'";
     $result = mysqli_query($connection, $query);
     confirmQuery($result);
 
-    if(mysqli_num_rows($result) > 0){
+    if (mysqli_num_rows($result) > 0) {
         return true;
     } else {
         return false;
     }
-
 }
 
-function redirect($location){
+function redirect($location)
+{
     header("Location:" . $location);
     exit;
 }
 
-function ifItIsMethod($method=null){
-    if($_SERVER['REQUEST_METHOD'] == strtoupper($method)){
+function ifItIsMethod($method = null)
+{
+    if ($_SERVER['REQUEST_METHOD'] == strtoupper($method)) {
         return true;
     }
     return false;
 }
-function isLoggedIn(){
-    if(isset($_SESSION['user_role'])){
+function isLoggedIn()
+{
+    if (isset($_SESSION['user_role'])) {
         return true;
     }
     return false;
 }
-function checkIfUserIsLoggedInAndRedirect($redirectLocation=null){
-    if(isLoggedIn()){
+function checkIfUserIsLoggedInAndRedirect($redirectLocation = null)
+{
+    if (isLoggedIn()) {
         redirect($redirectLocation);
     }
 }
 
-function register_user($username,$email,$password){
+function register_user($username, $email, $password)
+{
     global $connection;
-    
+
     $username = mysqli_real_escape_string($connection, $username);
     $email = mysqli_real_escape_string($connection, $email);
     $password = mysqli_real_escape_string($connection, $password);
     $password = password_hash($password, PASSWORD_DEFAULT);
     $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
     $query .= "VALUES('{$username}', '{$email}', '{$password}', 'subscriber' )";
-    $register_user_query = mysqli_query($connection,$query);
+    $register_user_query = mysqli_query($connection, $query);
     confirmQuery($register_user_query);
-
-    
 }
 
-function login_user($username, $password){
-   global $connection;
-   $username = trim($username);
-   $password = trim($password);
+function login_user($username, $password)
+{
+    global $connection;
+    $username = trim($username);
+    $password = trim($password);
     $username = mysqli_real_escape_string($connection, $username);
     $password = mysqli_real_escape_string($connection, $password);
     $query = "SELECT * FROM users WHERE username = '{$username}' ";
@@ -220,21 +229,49 @@ function login_user($username, $password){
         }
     }
     return true;
-    }
+}
 
-    function currentUser(){
-        if(isset($_SESSION['username'])){
-            return $_SESSION['username'];
-        }
-        return false;
+function currentUser()
+{
+    if (isset($_SESSION['username'])) {
+        return $_SESSION['username'];
     }
+    return false;
+}
 
 
-    function imagePlaceholder($image=''){
-        if(!$image){
-            return 'c++.png';
-        } else {
-            return $image;
-        }
+function imagePlaceholder($image = '')
+{
+    if (!$image) {
+        return 'c++.png';
+    } else {
+        return $image;
     }
-?>
+}
+function query($query)
+{
+    global $connection;
+    return mysqli_query($connection, $query);
+}
+function loggedInUserId()
+{
+    if (isLoggedIn()) {
+        $result = query("SELECT * FROM users WHERE username='" . $_SESSION['username'] . "'");
+        confirmQuery($result);
+        $user = mysqli_fetch_array($result);
+        return mysqli_num_rows($result) >= 1 ? $user['user_id'] : false;
+    }
+    return false;
+}
+function userLikedThisPost($post_id)
+{
+    $result = query("SELECT * FROM likes WHERE user_id=" . loggedInUserId() . " AND post_id={$post_id}");
+    confirmQuery($result);
+    return mysqli_num_rows($result) >= 1 ? true : false;
+}
+function getPostlikes($post_id)
+{
+    $result = query("SELECT * FROM likes WHERE post_id=$post_id");
+    confirmQuery($result);
+    echo mysqli_num_rows($result);
+}
--- index.php
+++ index.php
@@ -9,49 +9,42 @@
         <div class="col-md-8">
             <?php
             $per_page = 5;
-            if(isset($_GET['page'])){
+            if (isset($_GET['page'])) {
                 $page = $_GET['page'];
             } else {
                 $page = "";
             }
-            if($page == "" || $page == 1){
+            if ($page == "" || $page == 1) {
                 $page_1 = 0;
             } else {
                 $page_1 = ($page * $per_page) - $per_page;
             }
-            if(isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'admin'){
+            if (isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'admin') {
                 $post_query_count = "SELECT * FROM posts ";
-            } else{
+            } else {
                 $post_query_count = "SELECT * FROM posts WHERE post_status = 'published' ";
-
             }
             $find_count = mysqli_query($connection, $post_query_count);
             $count = mysqli_num_rows($find_count);
-            if($count < 1) {
+            if ($count < 1) {
                 echo '<h1 class="text-center">No Posts Available</h1>';
             } else {
-            $count = ceil($count / $per_page);
-
-
-
-
-            // $query = "SELECT * FROM posts LIMIT $page_1,$per_page";
-            $select_all_posts_query = mysqli_query($connection, $post_query_count);
-            while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                $post_id = $row['post_id'];
-                $post_title = $row['post_title'];
-                $post_author = $row['post_user'];
-                $post_date = $row['post_date'];
-                $post_image = $row['post_image'];
-                $post_content = substr($row['post_content'], 0, 100);
-                $post_status = $row['post_status'];
-                
+                $count = ceil($count / $per_page);
+                // $query = "SELECT * FROM posts LIMIT $page_1,$per_page";
+                $select_all_posts_query = mysqli_query($connection, $post_query_count);
+                while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                    $post_id = $row['post_id'];
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_user'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = substr($row['post_content'], 0, 100);
+                    $post_status = $row['post_status'];
             ?>
                     <h1 class="page-header">
                         Page Heading
                         <small>Secondary Text</small>
                     </h1>
-
                     <!-- First Blog Post -->
                     <h2>
                         <a href="post.php?p_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
@@ -60,7 +53,7 @@
                     <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
                     <hr>
                     <a href="post.php?p_id=<?php echo $post_id ?>">
-                    <img class="img-responsive" style="width:500px;" src="images/<?php echo imagePlaceholder($post_image) ?>" alt="">
+                        <img class="img-responsive" style="width:500px;" src="images/<?php echo imagePlaceholder($post_image) ?>" alt="">
                     </a>
                     <hr>
                     <p><?php echo $post_content ?></p>
@@ -68,7 +61,7 @@
                     <hr>
             <?php }
             }
-             ?>
+            ?>
         </div>
         <!-- Blog Sidebar Widgets Column -->
         <?php include "includes/sidebar.php"; ?>
@@ -77,14 +70,12 @@
     <hr>
     <ul class="pager">
         <?php
-        for($i = 1;$i<=$count;$i++){
-            if($i == $page){
+        for ($i = 1; $i <= $count; $i++) {
+            if ($i == $page) {
                 echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
             } else {
                 echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-
             }
-           
         }
         ?>
     </ul>
